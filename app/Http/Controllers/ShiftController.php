857695505aa2cd663acac3193156782bb48e832f
DIANA BERENICE VELAZQUEZ MOSCO <62391927+DBVM@users.noreply.github.com>
2024-12-09 22:38:45 -0600

    Starting shift crud with routes and shift controller

---
+++ app/Http/Controllers/ShiftController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ShiftController extends Controller
+{
+    public function index(){
+        return 'Hello from shift index';
+    }
+    public function create(){
+
+    }
+    public function store(){
+
+    }
+    public function edit(){
+
+    }
+    public function update(){
+
+    }
+    public function destroy(){
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ShiftController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -25,6 +26,14 @@ Route::middleware('auth')->group(function () {
     Route::get('users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
     Route::match(['put', 'patch'], 'users/{id}', [UserController::class, 'update'])->name('users.update');
     Route::delete('users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+
+    //SHIFTS
+    Route::get('shifts', [ShiftController::class, 'index'])->name('shifts');
+    Route::get('shifts/create', [ShiftController::class, 'create'])->name('shifts.create');
+    Route::post('shifts', [ShiftController::class, 'store'])->name('shifts.store');
+    Route::get('shifts/{id}/edit', [ShiftController::class, 'edit'])->name('shifts.edit');
+    Route::match(['put', 'patch'], 'shifts/{id}', [ShiftController::class, 'update'])->name('shifts.update');
+    Route::delete('shifts/{id}', [ShiftController::class, 'destroy'])->name('shifts.destroy');
 });
 
 
